8aaffa88c72d56f422f9e0e32a7d1626429612aa
laugh <laugh@email.com>
2023-08-11 23:37:18 +0800

    feat：upload组件功能完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="home-page">
+    <uploader
+      action="/api/upload"
+      :beforeUpload="beforeUpload"
+      @file-uploaded="onFileUploaded"
+    ></uploader>
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <img src="../assets/callout.svg" alt="callout" class="w-50" />
+          <h2 class="font-weight-light">随心写作，自由表达</h2>
+          <p>
+            <router-link to="/create" class="btn btn-primary my-2">开始写文章</router-link>
+          </p>
+        </div>
+      </div>
+    </section>
+    <h4 class="font-weight-bold text-center">发现精彩</h4>
     <column-list :list="list"></column-list>
+    <button class="btn btn-outline-primary mt-2 mb-5 mx-auto btn-block w-25 load-more">
+      加载更多
+    </button>
   </div>
 </template>
 
@@ -9,11 +29,15 @@ import { computed, defineComponent, onMounted } from 'vue'
 import { useStore } from 'vuex'
 import { GlobalDataProps } from '../store/index'
 import ColumnList from '../components/ColumnList.vue'
+import { ResponseType, ImageProps } from '@/store/testData'
+import Uploader from '@/base/Uploader.vue'
+import createMessage from '@/base/createMessage'
 
 export default defineComponent({
   name: 'HomeList',
   components: {
-    ColumnList
+    ColumnList,
+    Uploader
   },
   setup() {
     const store = useStore<GlobalDataProps>()
@@ -21,8 +45,20 @@ export default defineComponent({
       store.dispatch('fetchColumns')
     })
     const list = computed(() => store.state.columns)
+    const beforeUpload = (file: File) => {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        createMessage('上传图片只能是 JPG 格式', 'error')
+      }
+      return isJPG
+    }
+    const onFileUploaded = (rawData: ResponseType<ImageProps>) => {
+      createMessage(`上传图片ID ${rawData.data._id}`, 'success')
+    }
     return {
-      list: list
+      list,
+      beforeUpload,
+      onFileUploaded
     }
   }
 })
